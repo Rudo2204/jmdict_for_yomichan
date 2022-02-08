35be86a173edefab6ab92db5099c63ce2ab6ddea
Rudo2204 <rudo2204@gmail.com>
2022-02-08 16:30:06 +0700

    Add word frequency stats calculation feature

---
+++ src/frequency_stats.rs
@@ -0,0 +1,39 @@
+use crate::frequency_parser::WordFrequency;
+
+fn mean(data: &[f32]) -> Option<f32> {
+    let sum = data.iter().sum::<f32>();
+    let count = data.len();
+
+    match count {
+        positive if positive > 0 => Some(sum / count as f32),
+        _ => None,
+    }
+}
+fn std_deviation(data: &[f32]) -> Option<f32> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data
+                .iter()
+                .map(|value| {
+                    let diff = data_mean - (*value as f32);
+
+                    diff * diff
+                })
+                .sum::<f32>()
+                / count as f32;
+
+            Some(variance.sqrt())
+        }
+        _ => None,
+    }
+}
+
+pub fn get_freq_stats(vec_word_freq: Vec<WordFrequency>) -> (f32, f32) {
+    let data = vec_word_freq
+        .iter()
+        .map(|x| x.popularity)
+        .collect::<Vec<f32>>();
+    let data_mean = mean(&data).unwrap();
+    let data_std_deviation = std_deviation(&data).unwrap();
+    (data_mean, data_std_deviation)
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::path::PathBuf;
 use time::format_description::well_known::Rfc3339;
 
 mod frequency_parser;
+mod frequency_stats;
 mod jmdict_xml;
 
 pub const PROGRAM_NAME: &str = "jmdict_for_yomichan";
@@ -145,6 +146,11 @@ fn main() -> Result<()> {
     let input_path = matches.value_of("input").unwrap();
     let jmdict_xml = std::fs::read_to_string(input_path)?;
     let _ = jmdict_xml::process_jmdict(&jmdict_xml);
+    let raw_freq_input = std::fs::read_to_string("japanese-word-frequency/frequency.txt").unwrap();
+    let (_, vec_word_freq) =
+        frequency_parser::parse_frequency_input(&raw_freq_input.as_bytes()).unwrap();
+    let (mean, std_deviation) = frequency_stats::get_freq_stats(vec_word_freq);
+    info!("mean = {}, std_deviation = {}", mean, std_deviation);
 
     debug!("-----Everything is finished!-----");
     if lock {
