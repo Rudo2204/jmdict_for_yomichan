7d6e0adb701a460793b7e35d3e746befbbe1e98d
Rudo2204 <rudo2204@gmail.com>
2022-02-10 18:54:40 +0700

    Initial Term implementation

--- src/yomichan.rs
+++ src/yomichan.rs
@@ -39,24 +39,24 @@ struct Term {
     // If noun, return None
     identifiers: Identifier,
     popularity: f32,
-    definitions: String,
+    definitions: Definition,
     sequence_number: u32,
     //extra_tags: Option<String>,
 }
 
-impl Term {
-    fn serialize(&self) -> String {
-        format!(
-            r#"["{}","{}","","{}",{},["{}"],{},""]"#,
-            self.term,
-            self.reading,
-            self.identifiers.as_str(),
-            self.popularity,
-            self.definitions,
-            self.sequence_number
-        )
-    }
-}
+//impl Term {
+//    fn serialize(&self) -> String {
+//        format!(
+//            r#"["{}","{}","","{}",{},["{}"],{},""]"#,
+//            self.term,
+//            self.reading,
+//            self.identifiers.as_str(),
+//            self.popularity,
+//            self.definitions,
+//            self.sequence_number
+//        )
+//    }
+//}
 
 enum Identifier {
     Ichidan,
@@ -71,6 +71,75 @@ impl Identifier {
             Identifier::Godan => "v5",
             Identifier::Other => "",
         }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Definition {
+    // <keb> AKA multiple kanji form/ or multiple readings
+    term: Vec<String>,
+    // <reb>
+    reading: Vec<String>,
+    pos: Vec<Vec<String>>,
+    uk: bool,
+    gloss: Vec<Vec<String>>,
+    misc: Vec<Vec<String>>,
+}
+
+impl Definition {
+    fn serialize(&self) -> String {
+        "".to_string()
+    }
+    pub fn add_term(&mut self, term: String) -> &mut Self {
+        self.term.push(term);
+        self
+    }
+    pub fn add_reading(&mut self, reading: String) -> &mut Self {
+        self.reading.push(reading);
+        self
+    }
+    pub fn add_misc(&mut self, misc: String, sense: usize) -> &mut Self {
+        if self.misc.is_empty() {
+            self.misc = vec![vec![misc]];
+        } else if self.misc.len() == sense {
+            let mut sense = self.misc.pop().unwrap();
+            sense.push(misc);
+            self.misc.push(sense);
+        } else if self.misc.len() < sense {
+            let sense = vec![misc];
+            self.misc.push(sense);
+        }
+        self
+    }
+    pub fn add_pos(&mut self, pos: String, sense: usize) -> &mut Self {
+        if self.pos.is_empty() {
+            self.pos = vec![vec![pos]];
+        } else if self.pos.len() == sense {
+            let mut sense = self.pos.pop().unwrap();
+            sense.push(pos);
+            self.pos.push(sense);
+        } else if self.pos.len() < sense {
+            let sense = vec![pos];
+            self.pos.push(sense);
+        }
+        self
+    }
+    pub fn set_uk(&mut self) -> &mut Self {
+        self.uk = true;
+        self
+    }
+    pub fn add_gloss(&mut self, gloss: String, sense: usize) -> &mut Self {
+        if self.gloss.is_empty() {
+            self.gloss = vec![vec![gloss]];
+        } else if self.gloss.len() == sense {
+            let mut sense = self.gloss.pop().unwrap();
+            sense.push(gloss);
+            self.gloss.push(sense);
+        } else if self.gloss.len() < sense {
+            let sense = vec![gloss];
+            self.gloss.push(sense);
+        }
+        self
     }
 }
 
